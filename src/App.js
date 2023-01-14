0c096e34b42afbfa092a86a5aa9163b5e279df51
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-01-15 00:18:48 +0300

    add comments about setState

--- src/App.js
+++ src/App.js
@@ -24,8 +24,21 @@ class App extends Component {
             Hello {this.state.name.first}{this.state.name.last}!
           </p>
           <button onClick={() => {
-            this.setState({ name: { first: "React", last: "JSX" } });
-            console.log(this.state);
+            // this.setState({
+            //   name: { first: "React", last: "JSX" }
+            // }); // This runs asynchronously
+            // console.log(this.state); // This runs synchronously, so it will print the old state
+
+            this.setState(
+              (state, props) => { // state is equal to the current state
+                return {
+                  name: { first: "React", last: "JSX" }
+                }
+              },
+              () => { // This callback runs after the state is updated and optional
+                console.log(this.state);
+              }
+            );
           }}>
             Click Me!
           </button>
