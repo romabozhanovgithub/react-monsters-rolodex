db50d8deddd68f8b1dd92a3dec5024057d88dacb
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-01-15 01:47:32 +0300

    add list of monsters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,41 +7,54 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {
-        first: "React",
-        last: "JS"
-      }
+      monsters: [
+        {
+          id: 1,
+          name: "Frankenstein",
+        },
+        {
+          id: 2,
+          name: "Dracula",
+        },
+        {
+          id: 3,
+          name: "Zombie",
+        }, 
+      ]
     };
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello {this.state.name.first}{this.state.name.last}!
-          </p>
-          <button onClick={() => {
-            // this.setState({
-            //   name: { first: "React", last: "JSX" }
-            // }); // This runs asynchronously
-            // console.log(this.state); // This runs synchronously, so it will print the old state
+      // <p>
+      //   Hello {this.state.name.first}{this.state.name.last}!
+      // </p>
+      // <button onClick={() => {
+      //   // this.setState({
+      //   //   name: { first: "React", last: "JSX" }
+      //   // }); // This runs asynchronously
+      //   // console.log(this.state); // This runs synchronously, so it will print the old state
 
-            this.setState(
-              (state, props) => { // state is equal to the current state
-                return {
-                  name: { first: "React", last: "JSX" }
-                }
-              },
-              () => { // This callback runs after the state is updated and optional
-                console.log(this.state);
-              }
-            );
-          }}>
-            Click Me!
-          </button>
-        </header>
+      //   this.setState(
+      //     (state, props) => { // state is equal to the current state
+      //       return {
+      //         name: { first: "React", last: "JSX" }
+      //       }
+      //     },
+      //     () => { // This callback runs after the state is updated and optional
+      //       console.log(this.state);
+      //     }
+      //   );
+      // }}>
+      //   Click Me!
+      // </button>
+
+      <div className="App">
+        {
+          this.state.monsters.map(
+            monster => <h1 key={monster.id}>{monster.name}</h1>
+          )
+        }
       </div>
     );
   }
